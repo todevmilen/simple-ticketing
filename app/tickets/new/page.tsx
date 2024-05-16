cdec93ecc5cfabadd841652f4dc640250e6aa582
Milen Todev <todev@Milens-Laptop.local>
2024-05-16 21:53:23 +0300

    feat: add back button to ticket page

--- app/tickets/new/page.tsx
+++ app/tickets/new/page.tsx
@@ -1,10 +1,24 @@
+import { buttonVariants } from "@/components/ui/button";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const TicketForm = dynamic(() => import("@/components/TicketForm"), {
   ssr: false,
 });
 const NewTicket = () => {
-  return <TicketForm />;
+  return (
+    <div>
+      <div className="pb-3">
+        <Link
+          href="/tickets"
+          className={buttonVariants({ variant: "destructive", size: "sm" })}
+        >
+          Back
+        </Link>
+      </div>
+      <TicketForm />
+    </div>
+  );
 };
 
 export default NewTicket;
