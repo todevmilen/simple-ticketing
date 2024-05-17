5b088c46fe7ef0ecdfe447cb5acf465a5fa91c19
Milen Todev <todev@Milens-Laptop.local>
2024-05-17 21:50:36 +0300

    feat: style ticket details

--- app/tickets/[id]/TicketDetail.tsx
+++ app/tickets/[id]/TicketDetail.tsx
@@ -1,14 +1,78 @@
 import { Ticket } from "@prisma/client";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import TicketStatusBadge from "@/components/TicketStatusBadge";
+import TicketPriority from "@/components/ui/TicketPriority";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import ReactMarkDown from "react-markdown";
+
 interface Props {
   ticket: Ticket;
 }
 
 const TicketDetail = ({ ticket }: Props) => {
   return (
-    <div>
-      <p>{ticket.title}</p>
-      <p>{ticket.description}</p>
+    <div className="lg:grid lg:grid-cols-4">
+      <Card className="mx-4 mb-4 lg:col-span-3 lg:mr-4">
+        <CardHeader>
+          <div className="flex justify-between mb-3">
+            <TicketStatusBadge status={ticket.status} />
+            <TicketPriority priority={ticket.priority} />
+          </div>
+          <CardTitle>{ticket.title}</CardTitle>
+          <CardDescription>
+            Created:{" "}
+            {ticket.createdAt.toLocaleString("bg-BG", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            })}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="prose dark:prose-invert">
+          <ReactMarkDown>{ticket.description}</ReactMarkDown>
+        </CardContent>
+        <CardFooter>
+          Updated:{" "}
+          {ticket.updatedAt.toLocaleString("bg-BG", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          })}
+        </CardFooter>
+      </Card>
+      <div className="mx-4 flex lg:flex-col lg:mx-0 gap-2">
+        <Link
+          href={`/tickets/edit/${ticket.id}`}
+          className={`${buttonVariants({
+            variant: "default",
+          })}`}
+        >
+          Edit Ticket
+        </Link>
+        <Link
+          href={`/tickets/edit/${ticket.id}`}
+          className={`${buttonVariants({
+            variant: "default",
+          })}`}
+        >
+          Delete Ticket
+        </Link>
+      </div>
     </div>
   );
 };
