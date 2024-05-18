45f08935b926f45e3429bd8712ec45c42749b2b5
Milen Todev <todev@Milens-Laptop.local>
2024-05-18 21:26:04 +0300

    feat: implement sorting

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -6,18 +6,21 @@ import { buttonVariants } from "@/components/ui/button";
 import Pagination from "@/components/Pagination";
 import StatusFilter from "@/components/StatusFilter";
 import { Status } from "@prisma/client";
+import { Ticket } from "@prisma/client";
 
-interface SearchParams {
+export interface SearchParams {
   status: Status;
   page: string;
+  orderBy: keyof Ticket;
 }
 
 const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   const pageSize = 10;
   const page = parseInt(searchParams.page) || 1;
 
-  const statuses = Object.values(Status);
+  const orderBy = searchParams.orderBy ? searchParams.orderBy : "createdAt";
 
+  const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
@@ -37,6 +40,9 @@ const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   const ticketCount = await prisma.ticket.count({ where });
   const tickets = await prisma.ticket.findMany({
     where,
+    orderBy: {
+      [orderBy]: "desc",
+    },
     take: pageSize,
     skip: (page - 1) * pageSize,
   });
@@ -52,7 +58,7 @@ const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
         </Link>
         <StatusFilter />
       </div>
-      <DataTable tickets={tickets} />
+      <DataTable tickets={tickets} searchParams={searchParams} />
       <Pagination
         pageSize={pageSize}
         currentPage={page}
