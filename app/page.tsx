289090edd518029e382204a1a1aca411c188351f
Milen Todev <todev@Milens-Laptop.local>
2024-05-22 21:21:00 +0300

    feat: [dashboard] add recently updated card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
+import DashChart from "@/components/DashChart";
+import DashRecentTickets from "@/components/DashRecentTickets";
+import prisma from "@/prisma/db";
 import React from "react";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = async () => {
+  const tickets = await prisma.ticket.findMany({
+    where: {
+      NOT: [{ status: "CLOSED" }],
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    skip: 0,
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  const groupTicket = await prisma.ticket.groupBy({
+    by: ["status"],
+    _count: {
+      id: true,
+    },
+  });
+
+  console.log(groupTicket);
+
+  return (
+    <div>
+      <div className="grid gap-4 md:grid-cols-2 px-2">
+        <div>
+          <DashRecentTickets tickets={tickets} />
+        </div>
+        <div>
+          <DashChart />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ components/DashChart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashChart = () => {
+  return <div>DashChart</div>;
+};
+
+export default DashChart;
