3da4898f05b03fd0e04e78c0ed23d0e4a7b8d3f2
Milen Todev <todev@Milens-Laptop.local>
2024-05-22 20:56:28 +0300

    feat: protect api endpoints

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,8 +2,20 @@ import { userSchema } from "@/ValidationSchemas/users";
 import prisma from "@/prisma/db";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { getServerSession } from "next-auth";
+import options from "../auth/[...nextauth]/options";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
+  }
+
+  if (session.user.role !== "ADMIN") {
+    return NextResponse.json({ error: "Not Authorized" }, { status: 403 });
+  }
+
   const body = await request.json();
   const validation = userSchema.safeParse(body);
 
