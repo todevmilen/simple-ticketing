5bc46900f315d7333fb0667795a77b4edd05fc44
Milen Todev <todev@Milens-Laptop.local>
2024-05-22 21:05:14 +0300

    feat: protect pages

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,11 +1,18 @@
+import options from "@/app/api/auth/[...nextauth]/options";
 import UserForm from "@/components/UserForm";
 import prisma from "@/prisma/db";
+import { getServerSession } from "next-auth";
 
 interface Props {
   params: { id: string };
 }
 
 const EditUser = async ({ params }: Props) => {
+  const session = await getServerSession(options);
+  if (session?.user.role !== "ADMIN") {
+    return <p className="text-destructive">Admin access required.</p>;
+  }
+
   const user = await prisma.user.findUnique({
     where: {
       id: parseInt(params.id),
